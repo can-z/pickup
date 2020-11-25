c1e1994b183cb326eabe127651f0fe4e81ec9e52
Can Zhang <canzhang@hey.com>
2020-11-25 13:06:33 -0500

    list + button

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,63 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 const App: () => React$Node = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/home">
-          <h1>Home</h1>
-        </Route>
         <Route exact path="/">
+          <BatchListPage />
+        </Route>
+        <Route exact path="/getting-started">
           <StartingPoint />
         </Route>
       </Switch>
     </Router>
+  );
+};
+
+const BatchListPage: () => React$Node = () => {
+  let history = useHistory();
+
+  const goToDetailsPage: (batchId: string) => void = (batchId) => {
+    history.push("/getting-started");
+  };
+  return (
+    <div>
+      <button type="button" className="btn btn-primary">
+        New pickup
+      </button>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Date</th>
+            <th scope="col">Location</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr onClick={goToDetailsPage}>
+            <td>11-29 1PM - 1:30PM</td>
+            <td>4 Mallingham Crt, North York, ON</td>
+          </tr>
+          <tr onClick={goToDetailsPage}>
+            <td>11-29 2PM - 3:30PM</td>
+            <td>First Markham Place</td>
+          </tr>
+          <tr onClick={goToDetailsPage}>
+            <td>12-15 1PM - 1:30PM</td>
+            <td>Pacific Mall</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
